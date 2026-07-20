029872e6f5867177334fd6a15f901dac0a89e81d
shanza2495 <shanzarayan272@gmail.com>
2026-07-20 11:13:57 -0700

    Add Java program

---
+++ FirstClass.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class FirstClass {
+    public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in); 
+    System.out.println("Enter student name");
+    String name =sc.nextline();
+    System.out.println("Enter English marks");
+    int enslish = sc . nextInt();
+    System.out.println("Enter Math marks");
+    int math = sc . nextInt();
+    System.out.println("Enter Computer marks"); 
+    int total = english + math + computer ;
+    double percentage = total / 3.0;
+    System.out.println("Student Name: " + Name);
+System.out.println("Total marks: " + Marks);
+System.out.println("Percentage total: " + Percentage);
+if (percentage >=90) {
+    System.out.println("Grade: A");
+    
+}
+)
+
+    }
+}
+}
